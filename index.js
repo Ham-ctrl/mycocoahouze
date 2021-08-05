355eb7d08f4c4a3c9673833d293abd52ad4bf091
Ham-ctrl <54822006+Ham-ctrl@users.noreply.github.com>
2021-08-05 16:16:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 8000
 
-express()
+const app = express()  //express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(express.json());
+
+//Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'aboutIndexin' }));  //Here is displaying BUT when there is diff anada RO
+app.set('view engine', 'handlebars');
+
+//Handlebars the separated method
+
+//Body Parser
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'seconPublic')))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
