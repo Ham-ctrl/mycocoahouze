c9a55e25b5355aea92a9115f02f94ca79f747a10
Ham-ctrl <54822006+Ham-ctrl@users.noreply.github.com>
2021-08-05 16:21:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,4 @@ const app = express()  //express()
   app.get('/', (req, res) => res.render('pages/index'))
   //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.use(express.json());
-
-//Handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'aboutIndexin' }));  //Here is displaying BUT when there is diff anada RO
-app.set('view engine', 'handlebars');
-
-//Handlebars the separated method
-
-//Body Parser
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
-app.use(express.static(path.join(__dirname, 'seconPublic')))
-
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
