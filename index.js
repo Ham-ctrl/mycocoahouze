bd4105535c71427845521bdf8833ef04459ddaba
Ham-ctrl <54822006+Ham-ctrl@users.noreply.github.com>
2021-08-05 16:19:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 8000
 
 const app = express()  //express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
   //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.use(express.json());
